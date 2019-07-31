268aeb35b0bba1b7676a29ae7fb0904666b8b96f
plutotx <ptx.pluto@gmail.com>
2019-07-31 17:30:29 +0800

    add AlumSerialInterface

--- ros_serial_control/src/serial_send.py
+++ ros_serial_control/src/serial_send.py
@@ -5,9 +5,85 @@ import struct
 import serial
 import binascii
 from std_msgs.msg import Float32
-
+from sensor_msgs.msg import Joy
 
 # import time
+
+
+class AlumSerialInterface:
+
+    def __init__(self, port="/dev/ttyUSB0", BaudRate=9600):
+        rospy.loginfo("OK")
+        rospy.init_node("serial_send")
+        rospy.on_shutdown(self.shutdown)
+        try:
+            self.serialcon = serial.Serial(port, BaudRate, timeout=10)
+        except:
+            print("cant open")
+            raise
+        self.motor_cmd = 0
+        self.loop = rospy.Rate(20)
+        data = struct.pack('4B1H2B', 01, 04, 01, 01, 00, 13, 10)
+        rospy.loginfo(binascii.b2a_hex(data))
+
+    def send_A1(self, motor_cmd, motor_sign):
+        data = struct.pack('4B1H2B', 01, 04, 01, motor_sign, abs(motor_cmd), 13, 10)
+        rospy.loginfo(binascii.b2a_hex(data))
+        try:
+            self.serialcon.write(data)
+            rospy.loginfo("send successful")
+        except:
+            print("serial send failed!")
+            raise
+
+    def send_A2(self, motor_cmd, motor_sign):
+        data = struct.pack('4B1H2B', 01, 04, 02, motor_sign, abs(motor_cmd), 13, 10)
+        rospy.loginfo(binascii.b2a_hex(data))
+        try:
+            self.serialcon.write(data)
+            rospy.loginfo("send successful")
+        except:
+            print("serial send failed!")
+            raise
+
+    def send_A3(self, motor_cmd, motor_sign):
+        data = struct.pack('4B1H2B', 01, 04, 03, motor_sign, abs(motor_cmd), 13, 10)
+        rospy.loginfo(binascii.b2a_hex(data))
+        try:
+            self.serialcon.write(data)
+            rospy.loginfo("send successful")
+        except:
+            print("serial send failed!")
+            raise
+
+    def send_A4(self, motor_cmd, motor_sign):
+        data = struct.pack('4B1H2B', 01, 04, 04, motor_sign, 0, 13, 10)
+        rospy.loginfo(binascii.b2a_hex(data))
+        try:
+            self.serialcon.write(data)
+            rospy.loginfo("send successful")
+        except:
+            print("serial send failed!")
+            raise
+
+    def shutdown(self):
+        rospy.loginfo("Stopping")
+        data1 = struct.pack('4B1H2B', 01, 04, 01, 00, 0000, 13, 10)
+        data2 = struct.pack('4B1H2B', 01, 04, 02, 00, 0000, 13, 10)
+        data3 = struct.pack('4B1H2B', 01, 04, 03, 00, 0000, 13, 10)
+        data4 = struct.pack('4B1H2B', 01, 04, 04, 00, 0000, 13, 10)
+        rospy.loginfo(binascii.b2a_hex(data1))
+        rospy.loginfo(binascii.b2a_hex(data2))
+        rospy.loginfo(binascii.b2a_hex(data3))
+        rospy.loginfo(binascii.b2a_hex(data4))
+        try:
+            self.serialcon.write(data1)
+            self.serialcon.write(data2)
+            self.serialcon.write(data3)
+            self.serialcon.write(data4)
+            self.serialcon.close()
+        except:
+            pass
 
 
 class serialport_write():
@@ -140,6 +216,3 @@ if __name__ == '__main__':
         rospy.spin()
     except:
         raise
-
-
-# rospy.loginfo("send node terminated.")
